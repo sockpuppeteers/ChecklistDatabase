917039855f770ee7096b02b73580ff54e1b8c6d9
cademcniven <cademcniven@gmail.com>
2019-03-22 13:41:53 -0700

    updated database creation script
    Removed group/note tables and added a new table for recurring tasks. Added a couple data fields that were missing, and removed others that were unnecessary

--- buildDatabase.sql
+++ buildDatabase.sql
@@ -1,25 +1,15 @@
-USE master;
-GO
-
-IF  DB_ID('Checklist4all') IS NOT NULL
-    DROP DATABASE Checklist4all;
-GO
-
-CREATE DATABASE Checklist4all;
-GO
-
-USE Checklist4all;
-
 -- create the tables for the database
 CREATE TABLE Checklist (
   ChecklistID        INT            PRIMARY KEY   IDENTITY,
+  Name				 VARCHAR(30)
 );
 
 CREATE TABLE Users (
   UserID			INT            PRIMARY KEY   IDENTITY,
   Username			VARCHAR(25)    NOT NULL		 UNIQUE, 
   FName				NVARCHAR(20),
-  Lname				NVARCHAR(20)
+  Lname				NVARCHAR(20),
+  pw				VARCHAR(100)
 );
 
 CREATE TABLE Users_Checklist (
@@ -28,18 +18,19 @@ CREATE TABLE Users_Checklist (
   PRIMARY KEY(UserID, ChecklistID)
 );
 
---Deadline and date completed are strings in a certain date format, so they should always be the same length (or null)
---which means varchar is unnecessary. idk the length of the date format we're using though, so it can be changed later
 CREATE TABLE Task (
   TaskID				INT            PRIMARY KEY  IDENTITY,
   ChecklistID			INT            REFERENCES Checklist (ChecklistID),
   Name					VARCHAR(40)    NOT NULL,
-  Description			VARCHAR(200),
-  HasDeadline			BIT			   NOT NULL,
   Deadline				VARCHAR(15),
-  Completed				BIT			   NOT NULL,
-  DateCompleted			varchar(15),
-  CompletedBy			INT			   REFERENCES Users(UserID)
+  DateCompleted			VARCHAR(15),
+  IsRecurring			BIT
+);
+
+CREATE TABLE RecurringTask (
+	TaskID				INT			REFERENCES Task(TaskID),
+	Day					VARCHAR(10),
+	Time				VARCHAR(10)
 );
 
 CREATE TABLE Change (
@@ -60,23 +51,23 @@ CREATE TABLE Friends (
 
 --This is to let users leave a ~50 character note on other people's profile,
 --only visible to the person who created the note
-CREATE TABLE Notes (
-	UserID		INT		REFERENCES Users(UserID),
-	VictimID	INT		REFERENCES Users(UserID),
-	PRIMARY KEY(UserID, VictimID),
-	Note		VARCHAR(50)
-);
+--CREATE TABLE Notes (
+--	UserID		INT		REFERENCES Users(UserID),
+--	VictimID	INT		REFERENCES Users(UserID),
+--	PRIMARY KEY(UserID, VictimID),
+--	Note		VARCHAR(50)
+--);
 
-CREATE TABLE Groups (
-	GroupID		INT		PRIMARY KEY		IDENTITY,
-	Name		VARCHAR(30)
-);
+--CREATE TABLE Groups (
+--	GroupID		INT		PRIMARY KEY		IDENTITY,
+--	Name		VARCHAR(30)
+--);
 
-CREATE TABLE Group_Users (
-	GroupID		INT		REFERENCES Groups(GroupID),
-	UserID		INT		REFERENCES Users(UserID),
-	PRIMARY KEY(GroupID, UserID)
-);
+--CREATE TABLE Group_Users (
+--	GroupID		INT		REFERENCES Groups(GroupID),
+--	UserID		INT		REFERENCES Users(UserID),
+--	PRIMARY KEY(GroupID, UserID)
+--);
 
 --This will make the primary key for users start at 100M
 --This is done to make sure there are no issues with the composite pk for user_checklist
